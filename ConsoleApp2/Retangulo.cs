74f5f290f59cff1ca0cab40b4d9e7b3db0af0a94
Douglas Andrade <douglas.ftelecom@yahoo.com.br>
2020-06-13 12:18:56 -0300

    Retangulo

---
+++ ConsoleApp2/Retangulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Retangulo
+    {
+        public double largura;
+        public double altura;
+
+        public double Area()
+        {
+            return (largura * altura);
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (largura + altura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(largura,2) + (Math.Pow(altura,2)));
+        }
+    }
+}
